332506fc50fc58e4bff5caa8a95f8ea0cbf2731b
Biniyam Erana <36880225+BiniyamErana@users.noreply.github.com>
2021-03-15 13:35:19 -0500

    Lab12

---
+++ script/maze.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	var start = false;
+	$("#start").click(function() {
+		start = true;
+		$("#status").text("Game Started! ");
+		$(".boundary").removeClass("youlose");
+	});
+	$(".boundary").mouseover(function() {
+		lose();
+	});
+
+	$("body").mouseover(function() {
+		if (event.target.id != ("maze") && event.target.id != ("start") && event.target.id != ("end")) {
+			lose();
+		}
+	});
+
+	function lose() {
+		if (start) {
+			start = false;
+			$("#status").text("You lose!");
+			$(".boundary").addClass("youlose");
+		}
+	}
+	$("#end").mouseover(function() {
+		if (start) {
+			start = false;
+			$("#status").text("You win!");
+		}
+	});
+
+});
